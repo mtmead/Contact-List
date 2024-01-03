e915dd3cb21412996ae302b6b9bef8c98ce071de
Marianna Mead <mariannatmead@gmail.com>
2024-01-03 16:44:00 -0500

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,32 @@
 
 import './App.css'
+import './components/ContactList'
+
+import React, { useState } from 'react';
+
+function Contacts() {
+  const [contacts, setContacts] = useState(dummyContacts)
+}
+
+export default function ContactList() { 
+
+  return ( 
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="3">Contact List</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Phone</td>
+            </tr>
+            {
+               // Map over data here
+             }
+          </tbody>
+        </table>
+    ); 
+}
